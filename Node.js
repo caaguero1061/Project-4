645ea787fabdec0f07af10e6fae34fc6058d9544
caaguero1061 <cassy13a@gmail.com>
2025-12-29 01:01:42 -0600

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,1 +1,29 @@
+bootcampbuddy
+│
+├─ src
+│  ├─ app.js
+│  ├─ config
+│  │   └─ db.js
+│  ├─ middleware
+│  │   ├─ auth.js
+│  │   └─ error.js
+│  ├─ models
+│  │   ├─ User.js
+│  │   ├─ Topic.js
+│  │   ├─ Question.js
+│  │   └─ Answer.js
+│  ├─ routes
+│  │   ├─ auth.js
+│  │   ├─ topics.js
+│  │   ├─ questions.js
+│  │   └─ answers.js
+│  ├─ seed
+│  │   └─ seed.js
+│  └─ utils
+│      └─ tokens.js
+│
+├─ .env
+├─ .env.example
+├─ package.json
+└─ README.md
 
